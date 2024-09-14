1ddbcc08f1a722289bd81f5638175b2e722b226a
milon-unit <tcompany560@gmail.com>
2024-09-14 13:14:29 +0600

    first commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\paperbox02\env
+set VIRTUAL_ENV=C:\xampp\htdocs\paperbox02\env
 
 if not defined PROMPT set PROMPT=$P$G
 
